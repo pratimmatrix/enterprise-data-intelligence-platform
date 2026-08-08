ea52d2fd0624055fb32bab65c999e42bd853ce87
PRATIM MISTRY <pratimofficial108@gmail.com>
2026-08-08 18:33:02 +0530

    Add reusable data profiling engine

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from src.ingestion.loader import DataLoader
 from src.ingestion.validator import DataValidator
 from src.profiling.metadata import MetadataExtractor
+from src.profiling.profiler import DataProfiler
 
 
 def main():
 
     loader = DataLoader()
 
-    df = loader.load("data/raw/employees.xlsx")
+    df = loader.load("data/raw/bank-full.csv")
 
     validator = DataValidator()
     validator.validate(df)
 
     metadata = MetadataExtractor()
     metadata.extract(df)
+
+    profiler = DataProfiler()
+
+    profile = profiler.profile(df)
+
+    profiler.display(profile)
 
 
 if __name__ == "__main__":
